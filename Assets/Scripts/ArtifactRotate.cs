a10df066c9fa22f79b18aa9b13adfd3ef0d1bbd8
emmanuelthegrey <emmanuelthegrey@gmail.com>
2020-05-11 23:26:43 -0400

    Rotating cube
    Cube is rotating on wasd

---
+++ Assets/Scripts/ArtifactRotate.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtifactRotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            // transform.Rotate(Vector3.up, 1.0f);
+            transform.RotateAround(new Vector3(0f, 0f, 0f), Vector3.up, 1.0f);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            //transform.Rotate(Vector3.down, 1.0f);
+            transform.RotateAround(new Vector3(0f, 0f, 0f), new Vector3(0f, 1f, 0f), 1.0f);
+
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            //transform.Rotate(Vector3.right, 1.0f);
+
+            transform.RotateAround(new Vector3(0f, 0f, 0f), Vector3.right, 1.0f);
+
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            //transform.Rotate(Vector3.left, 1.0f);
+
+            transform.RotateAround(new Vector3(0f, 0f, 0f), Vector3.left, 1.0f);
+
+        }
+    }
+}
